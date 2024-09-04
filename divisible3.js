627040acc576acda6eaceb516266db68e6b6f481
Vamshi12m <vamshi.maddepally123@gmail.com>
2024-09-05 00:40:05 +0530

    day3

---
+++ divisible3.js
@@ -0,0 +1,12 @@
+console.log("1st method");
+i="";
+for(i=3;i<=30;i=i+3){
+    console.log(i);
+}
+console.log("2nd method");
+
+for(j=1;j<=30;j++){
+    if(j%3==0){
+        console.log(j);
+    }
+}
---
+++ mul-table.js
@@ -0,0 +1,4 @@
+var table5=5;
+for(i=1;i<=10;i++){
+    console.log(table5+" x "+i+" = "+i*table5);
+}
